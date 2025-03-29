7d8640bda4e039af535aba9b6324e451848cdfee
Jennytt <jennytoy7661@gmail.com>
2025-03-29 19:28:07 +0800

    feat: add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,9 @@ import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -14,11 +17,30 @@ export default [
 	prettier,
 	...svelte.configs['flat/prettier'],
 	{
+		plugins: {
+			'unused-imports': eslintPluginUnusedImports,
+			prettier: eslintPluginPrettier
+		},
 		languageOptions: {
 			globals: {
 				...globals.browser,
 				...globals.node
 			}
+		},
+		rules: {
+			'import/named': 'off',
+			'no-console': ['warn', { allow: ['warn', 'error'] }],
+			'unused-imports/no-unused-imports': 'error',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePattern: '^_',
+					args: 'after-used',
+					argsIgnorePattern: '^_'
+				}
+			],
+			'prettier/prettier': 'error'
 		}
 	}
 ];
