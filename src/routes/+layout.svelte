a7ab45d60a33ef681c0db7e722308c93b9bb5ddd
Jennytt <jennytoy7661@gmail.com>
2025-03-30 19:44:22 +0800

    feat: add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css';
+	import NavBar from '../lib/customComponents/NavBar.svelte';
 	let { children } = $props();
 </script>
 
+<NavBar />
 {@render children()}
+<!-- <Footer /> -->
