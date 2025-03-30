97c74474ce2dddbe190613595055486e6b80e1a3
Jennytt <jennytoy7661@gmail.com>
2025-03-30 20:15:47 +0800

    feat: install lucide and change nav icon

--- src/lib/customComponents/NavBar.svelte
+++ src/lib/customComponents/NavBar.svelte
@@ -1,5 +1,6 @@
 <script>
 	import logoSmall from '../../assets/MOicon-small.png';
+	import { Search, Gem, Bookmark, UserRound, CircleUser } from '@lucide/svelte';
 </script>
 
 <div class="navbar-unsigned">
@@ -14,93 +15,27 @@
 
 		<div class="nav-searchbar">
 			<div class="what-do-you-want-to-search">What do you want to search?</div>
-			<svg
-				class="icons"
-				width="25"
-				height="24"
-				viewBox="0 0 25 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<g clip-path="url(#clip0_846_9483)">
-					<path
-						d="M16.4588 14.0006H15.6688L15.3888 13.7306C16.5888 12.3306 17.2088 10.4206 16.8688 8.39063C16.3988 5.61063 14.0788 3.39063 11.2788 3.05063C7.04876 2.53063 3.48876 6.09063 4.00876 10.3206C4.34876 13.1206 6.56876 15.4406 9.34876 15.9106C11.3788 16.2506 13.2888 15.6306 14.6888 14.4306L14.9588 14.7106V15.5006L19.2088 19.7506C19.6188 20.1606 20.2888 20.1606 20.6988 19.7506C21.1088 19.3406 21.1088 18.6706 20.6988 18.2606L16.4588 14.0006ZM10.4588 14.0006C7.96876 14.0006 5.95876 11.9906 5.95876 9.50063C5.95876 7.01063 7.96876 5.00063 10.4588 5.00063C12.9488 5.00063 14.9588 7.01063 14.9588 9.50063C14.9588 11.9906 12.9488 14.0006 10.4588 14.0006Z"
-						fill="#D1D7E0"
-					/>
-				</g>
-				<defs>
-					<clipPath id="clip0_846_9483">
-						<rect width="24" height="24" fill="white" transform="translate(0.95813)" />
-					</clipPath>
-				</defs>
-			</svg>
+			<Search color="#D1D7E0" />
 		</div>
 
 		<div class="nav-pro">
-			<svg
-				class="vector4"
-				width="25"
-				height="20"
-				viewBox="0 0 25 20"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M5.68533 0.61853H19.9396C20.2595 0.61853 20.5601 0.77161 20.7483 1.03035L24.5711 6.28675C24.7114 6.47963 24.6959 6.74473 24.5342 6.91997L13.1798 19.2205C12.9925 19.4234 12.6762 19.4361 12.4733 19.2488C12.4668 19.2428 8.6726 15.1332 1.09073 6.91997C0.928965 6.74473 0.913489 6.47963 1.05377 6.28675L4.8766 1.03035C5.06478 0.77161 5.3654 0.61853 5.68533 0.61853ZM6.19456 2.61853L3.39512 6.46776L12.8124 16.6699L22.2298 6.46776L19.4303 2.61853H6.19456Z"
-					fill="#B693DC"
-				/>
-			</svg>
+			<Gem color="#B693DC" />
+
 			<div class="pro">PRO</div>
 		</div>
 
 		<div class="nav-watchlist">
-			<svg
-				class="bookmark-3-line"
-				width="25"
-				height="24"
-				viewBox="0 0 25 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M4.66681 2H20.6668C21.2191 2 21.6668 2.44772 21.6668 3V22.2763C21.6668 22.5525 21.4429 22.7764 21.1668 22.7764C21.0966 22.7764 21.0272 22.7615 20.9631 22.7329L12.6668 19.0313L4.37054 22.7329C4.11836 22.8455 3.82272 22.7322 3.7102 22.4801C3.68159 22.4159 3.66681 22.3465 3.66681 22.2763V3C3.66681 2.44772 4.11453 2 4.66681 2ZM19.6668 19.9645V4H5.66681V19.9645L12.6668 16.8412L19.6668 19.9645ZM12.6668 13.5L9.72788 15.0451L10.2892 11.7725L7.91153 9.45492L11.1973 8.97746L12.6668 6L14.1363 8.97746L17.4221 9.45492L15.0444 11.7725L15.6057 15.0451L12.6668 13.5Z"
-					fill="#D1D7E0"
-				/>
-			</svg>
+			<Bookmark color="#D1D7E0" />
 			<div class="watchlist">Watchlist</div>
 		</div>
 
 		<div class="nav-account">
-			<svg
-				class="user-line"
-				width="25"
-				height="24"
-				viewBox="0 0 25 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M4.66675 22C4.66675 17.5817 8.24847 14 12.6667 14C17.085 14 20.6667 17.5817 20.6667 22H18.6667C18.6667 18.6863 15.9804 16 12.6667 16C9.35304 16 6.66675 18.6863 6.66675 22H4.66675ZM12.6667 13C9.35175 13 6.66675 10.315 6.66675 7C6.66675 3.685 9.35175 1 12.6667 1C15.9817 1 18.6667 3.685 18.6667 7C18.6667 10.315 15.9817 13 12.6667 13ZM12.6667 11C14.8767 11 16.6667 9.21 16.6667 7C16.6667 4.79 14.8767 3 12.6667 3C10.4567 3 8.66675 4.79 8.66675 7C8.66675 9.21 10.4567 11 12.6667 11Z"
-					fill="#D1D7E0"
-				/>
-			</svg>
+			<UserRound color="#D1D7E0" />
 			<div class="log-in">Log in</div>
 		</div>
 
 		<div class="nav-account">
-			<svg
-				class="account-circle-2-line"
-				width="25"
-				height="24"
-				viewBox="0 0 25 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M12.6667 22C7.1439 22 2.66675 17.5228 2.66675 12C2.66675 6.47715 7.1439 2 12.6667 2C18.1895 2 22.6667 6.47715 22.6667 12C22.6667 17.5228 18.1895 22 12.6667 22ZM12.6667 20C17.085 20 20.6667 16.4183 20.6667 12C20.6667 7.58172 17.085 4 12.6667 4C8.24847 4 4.66675 7.58172 4.66675 12C4.66675 16.4183 8.24847 20 12.6667 20ZM12.6667 8C13.219 8 13.6667 8.44772 13.6667 9C13.6667 9.55228 13.219 10 12.6667 10C12.1144 10 11.6667 9.55228 11.6667 9C11.6667 8.44772 12.1144 8 12.6667 8ZM12.6667 12C14.3236 12 15.6667 10.6569 15.6667 9C15.6667 7.34315 14.3236 6 12.6667 6C11.0098 6 9.66675 7.34315 9.66675 9C9.66675 10.6569 11.0098 12 12.6667 12ZM12.6667 15C11.5621 15 10.6667 15.8954 10.6667 17H8.66675C8.66675 14.7909 10.4576 13 12.6667 13C14.8758 13 16.6667 14.7909 16.6667 17H14.6667C14.6667 15.8954 13.7713 15 12.6667 15Z"
-					fill="#D1D7E0"
-				/>
-			</svg>
+			<CircleUser color="#D1D7E0" />
 			<div class="sign-up">Sign Up</div>
 		</div>
 	</div>
@@ -164,14 +99,6 @@
 		position: relative;
 	}
 
-	.icons {
-		flex-shrink: 0;
-		width: 24px;
-		height: 24px;
-		position: relative;
-		overflow: visible;
-	}
-
 	.nav-pro {
 		border-radius: 20px;
 		display: flex;
@@ -182,14 +109,6 @@
 		flex-shrink: 0;
 		position: relative;
 		cursor: pointer;
-	}
-
-	.vector4 {
-		flex-shrink: 0;
-		width: 23.71px;
-		height: 18.76px;
-		position: relative;
-		overflow: visible;
 	}
 
 	.pro {
@@ -215,16 +134,8 @@
 		cursor: pointer;
 	}
 
-	.bookmark-3-line {
-		flex-shrink: 0;
-		width: 24px;
-		height: 24px;
-		position: relative;
-		overflow: visible;
-	}
-
 	.watchlist {
-		color: #ffffff;
+		color: #d1d7e0;
 		text-align: center;
 		font-family: 'Inter-SemiBold', sans-serif;
 		font-size: 16px;
@@ -244,14 +155,6 @@
 		cursor: pointer;
 	}
 
-	.user-line {
-		flex-shrink: 0;
-		width: 24px;
-		height: 24px;
-		position: relative;
-		overflow: visible;
-	}
-
 	.log-in {
 		color: var(--brand-nav-bar-icon-grey, #d1d7e0);
 		text-align: center;
@@ -260,14 +163,6 @@
 		line-height: 140%;
 		font-weight: 600;
 		position: relative;
-	}
-
-	.account-circle-2-line {
-		flex-shrink: 0;
-		width: 24px;
-		height: 24px;
-		position: relative;
-		overflow: visible;
 	}
 
 	.sign-up {
