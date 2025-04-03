f6ad5d3db9b7c773edcbc96c0c21b94b3af4a4b8
Jennytt <jennytoy7661@gmail.com>
2025-04-03 16:50:31 +0800

    fix: add nav bar navigation and fix search bar

--- src/lib/customComponents/NavBar.svelte
+++ src/lib/customComponents/NavBar.svelte
@@ -1,27 +1,46 @@
 <script>
 	import logoSmall from '../../assets/MOicon-small.png';
 	import { Search, Gem, Bookmark, UserRound, CircleUser } from '@lucide/svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+
+	// ! TODO: Function to handle search click
+	function handleSearchClick() {
+		// const searchInput = document.querySelector('input[type="text"]');
+		// 		const query = searchInput.value;
+	}
 </script>
 
 <div class="relative box-border h-[66px] overflow-hidden bg-[#1d1d1d]">
 	<div
 		class="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-row items-center justify-start gap-[50px]"
 	>
+		<!-- MovieOverflow icon -->
 		<div class="relative flex flex-shrink-0 flex-row items-center justify-start">
-			<img
-				class="h-[44px] min-h-[44px] w-[180px] min-w-[180px]"
-				alt="The project logo"
-				src={logoSmall}
-			/>
+			<a href="/"
+				><img
+					class="h-[44px] min-h-[44px] w-[180px] min-w-[180px]"
+					alt="The project logo"
+					src={logoSmall}
+				/></a
+			>
 		</div>
-
-		<div
-			class="relative flex h-[56px] w-[514px] min-w-[250px] flex-shrink-0 flex-row items-center justify-between rounded-[10px] bg-[#2b2b2b] px-1.5 py-3"
-		>
-			<div class="relative text-left text-sm font-normal text-[#797979]">
-				What do you want to search?
+		<div class="relative">
+			<Input
+				class="relative h-[56px] w-[514px] min-w-[250px] flex-shrink-0 flex-row items-center justify-between rounded-[10px] border-[#2B2B2B] bg-[#2b2b2b] px-4 py-3 pr-12 text-white"
+				placeholder="What do you want to search?"
+				type="text"
+			/>
+			<!-- Positioned the search icon at the right side of the search bar -->
+			<div
+				class="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
+				on:click={handleSearchClick}
+				on:keydown={(e) => e.key === 'Enter' && handleSearchClick()}
+				tabindex="0"
+				role="button"
+				aria-label="Search"
+			>
+				<a href="/search"><Search color="#D1D7E0" size={20} /></a>
 			</div>
-			<Search color="#D1D7E0" />
 		</div>
 
 		<div
@@ -31,7 +50,7 @@
 			<div
 				class="relative h-[22px] w-[45px] text-left text-base font-semibold leading-[140%] text-[#b693dc]"
 			>
-				PRO
+				<a href="/pro">PRO</a>
 			</div>
 		</div>
 
@@ -40,7 +59,7 @@
 		>
 			<Bookmark color="#D1D7E0" />
 			<div class="relative text-center text-base font-semibold leading-[140%] text-[#d1d7e0]">
-				Watchlist
+				<a href="/watchlist">Watchlist</a>
 			</div>
 		</div>
 
@@ -49,7 +68,7 @@
 		>
 			<UserRound color="#D1D7E0" />
 			<div class="relative text-center text-base font-semibold leading-[140%] text-[#d1d7e0]">
-				Log in
+				<a href="login">Log in</a>
 			</div>
 		</div>
 
@@ -58,7 +77,7 @@
 		>
 			<CircleUser color="#D1D7E0" />
 			<div class="relative text-center text-base font-semibold leading-[140%] text-[#d1d7e0]">
-				Sign Up
+				<a href="signup">Sign Up</a>
 			</div>
 		</div>
 	</div>
