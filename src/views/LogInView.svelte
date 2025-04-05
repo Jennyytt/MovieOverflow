30fc3faac63a19c692ee8cc32e7be99adfee76ad
c480-lemon <florindadaluz@gmail.com>
2025-04-05 17:33:11 +0800

    feat: Log In Page added

---
+++ src/views/LogInView.svelte
@@ -0,0 +1,17 @@
+<script>
+    import LogInWindow from '../lib/customComponents/userauthen/LogInBox.svelte';
+    import logoBig from '../assets/MOicon-large.png'
+</script>
+
+<!-- Full page layout: horizontally align logo + login box -->
+<div class="flex min-h-screen items-center justify-center gap-[34px] bg-black px-8 py-12">
+	<!-- Logo Section -->
+	<div class="flex flex-col items-start">
+		<img src={logoBig} alt="MovieOverflow logo" class="w-[401px] h-[148px]" />
+	</div>
+
+	<!-- Login Section -->
+	<div>
+		<LogInWindow />
+	</div>
+</div>
