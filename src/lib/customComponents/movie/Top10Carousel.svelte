ebc33af5e3b5ec96beab8407b8f5b526dc6fdc8e
Tonball112 <tommylau120@gmail.com>
2025-04-13 02:16:55 +0800

    feat: add homepage

---
+++ src/lib/customComponents/movie/Top10Carousel.svelte
@@ -0,0 +1,31 @@
+<script>
+	import * as Carousel from '$lib/components/ui/carousel/index.js';
+	import Top10MovieCard from './Top10MovieCard.svelte';
+</script>
+
+<Carousel.Root
+	opts={{
+		align: 'start'
+	}}
+	class="relative left-10 w-[1070px] max-w-full pl-4"
+>
+	<div>
+		<Carousel.Previous class="mx-[16px]" />
+		<Carousel.Content class="-ml-4">
+			<!-- eslint-disable-next-line no-unused-vars -->
+			{#each Array(10) as _, i (i)}
+				<Carousel.Item class="flex items-start pl-3 md:basis-1/2 lg:basis-1/3">
+					<!-- Ranking and purple line (horizontal) -->
+					<div class="mr-2 flex items-center">
+						<div class="text-2xl font-bold text-white">{i + 1}</div>
+						<div
+							class="relative ml-2 h-[37px] w-[5px] flex-shrink-0 rounded-[2px] bg-[#802bb1]"
+						></div>
+					</div>
+					<Top10MovieCard rank={i + 1} />
+				</Carousel.Item>
+			{/each}
+		</Carousel.Content>
+		<Carousel.Next />
+	</div>
+</Carousel.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import TrendingMovieView from '../views/homepage/TrendingMovieView.svelte';
+	import UpComingMovieView from '../views/homepage/UpComingMovieView.svelte';
+	import LatestCriticsView from '../views/homepage/LatestCriticsView.svelte';
+	import Top10MovieView from '../views/homepage/Top10MovieView.svelte';
+	import BestMovieWithTopPicksView from '../views/homepage/BestMovieWithTopPicksView.svelte';
+</script>
+
+<div>
+	<BestMovieWithTopPicksView />
+	<br />
+	<br />
+	<TrendingMovieView />
+	<br />
+	<br />
+	<UpComingMovieView />
+	<br />
+	<br />
+	<LatestCriticsView />
+	<br />
+	<br />
+	<Top10MovieView />
+	<br />
+	<br />
+</div>
