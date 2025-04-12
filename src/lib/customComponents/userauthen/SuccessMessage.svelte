6545308a09baa30825085e02cae7f60f194ffa5b
c480-lemon <florindadaluz@gmail.com>
2025-04-12 23:11:33 +0800

    feat: create custom component Success Message

---
+++ src/lib/customComponents/userauthen/SuccessMessage.svelte
@@ -0,0 +1,63 @@
+<script>
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import { Cross2 } from "radix-icons-svelte";
+
+  export let messages = [];
+  export let title = "Successful";
+
+  function handleClose() {
+    console.log("Close button clicked");
+  }
+
+  function goToLogin() {
+    window.location.href = "/login";
+  }
+</script>
+
+<Card.Root
+  class="relative w-[527px] min-h-[600px] rounded-[23.48px] border-[3px] border-[#ABBED1] bg-[#F8F8F8] pt-[75px] px-[42px] pb-[68px]"
+  style="outline-offset: -3px"
+>
+  <!-- Close Icon -->
+  <Cross2
+    class="absolute top-[24px] right-[24px] w-5 h-5 text-[#666] cursor-pointer"
+    on:click={handleClose}
+  />
+
+  <Card.Content class="flex flex-col gap-[25px] p-0 w-full">
+    <!-- Title Section -->
+    <div class="flex items-start gap-[8px]">
+      <div class="w-[4.25px] h-[31.31px] bg-[#A508FF] rounded-[2px]" />
+      <h1 class="text-[#46344E] text-[31.31px] font-black leading-none">
+        {title}
+      </h1>
+    </div>
+
+    <div class="h-[80px]" />
+    
+    <!-- Messages -->
+    <div class="w-[444px] text-[#5F1F73] text-[15px] leading-[24px] tracking-[0.15px]">
+      {#each messages as msg}
+        <p>{msg}</p>
+      {/each}
+    </div>
+
+    <!-- Spacer -->
+    <div class="h-[80px]" />
+
+    <!-- Button -->
+    <div class="h-[55px]">
+      <div
+        on:click={goToLogin}
+        class="cursor-pointer w-[449.04px] h-[54.78px] bg-[rgba(128,43,177,0.80)] rounded-[31.31px] relative overflow-hidden"
+      >
+        <div
+          class="absolute left-1/2 -translate-x-1/2 top-[16.5px] text-white text-[17.61px] font-[400] text-center"
+        >
+          Log in
+        </div>
+      </div>
+    </div>
+  </Card.Content>
+</Card.Root>
