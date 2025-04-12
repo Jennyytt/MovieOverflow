fcd8167a0977e29761ebae07909bd97b91f4cd74
c480-lemon <florindadaluz@gmail.com>
2025-04-12 22:52:19 +0800

    fix: add reusable component SuccessMessage

--- src/routes/signup/success/+page.svelte
+++ src/routes/signup/success/+page.svelte
@@ -1,9 +1,16 @@
 <script>
-	import SuccessMessage from '../../../views/SignUpSuccess.svelte';
+	import SignUpSuccess from '/Users/florinda/MovieOverflow/src/lib/customComponents/userauthen/SuccessMessage.svelte';
+	
+
 	
 </script>
 
 <div>
-	<SuccessMessage/>
+	<SignUpSuccess
+  messages={[
+    "Congratulations! You have created an account.",
+	"Click “Log in” to log in MovieOverflow."
+  ]}
+/>
 	<br />
 </div>
