d2096853e64e865430429cf646896b1411f748e4
Jeffrey Law <jeffreylaw@Jeffreys-MacBook-Air.local>
2025-04-14 06:05:14 +0800

    feat: fix  Movie Critics Review Page

--- src/lib/customComponents/review/LargerReviewCard.svelte
+++ src/lib/customComponents/review/LargerReviewCard.svelte
@@ -3,29 +3,51 @@
 	import { CircleUserRound } from '@lucide/svelte';
 	export let username = 'User Name';
 	export let date = 'Feb 24, 2025';
+	export let reviewTitle = 'Title Of Critics Review';
+	export let reviewText =
+		'Good movie!<br/>Good!<br/>Good!<br/>Good!<br/>Good movie but have a lot of potential to be even mor...';
 </script>
 
-<div class="flex h-full w-full gap-[61px]">
-	<div class="flex w-[163px] flex-col gap-3">
-		<div class="inline-flex h-[41.99px] items-center gap-[18px] self-stretch">
-			<div class="relative aspect-square h-[42.34px] w-[42.34px] flex-shrink-0">
-				<CircleUserRound class="h-full w-full" color="#D1D7E0" />
+<div class="flex w-full flex-col items-start gap-6">
+	<div class="flex h-full w-full gap-[61px]">
+		<div class="flex w-[163px] flex-col gap-3">
+			<div class="inline-flex h-[41.99px] items-center gap-[18px] self-stretch">
+				<div class="relative aspect-square h-[42.34px] w-[42.34px] flex-shrink-0">
+					<CircleUserRound class="h-full w-full" color="#D1D7E0" />
+				</div>
+				<div>
+					<span
+						class="overflow-hidden whitespace-nowrap break-normal font-inter text-[18.41px] font-medium text-[#cccccc]"
+						>{username}</span
+					>
+				</div>
 			</div>
 			<div>
-				<span class="break-words font-inter text-[18.41px] font-medium text-[#cccccc]"
-					>{username}</span
-				>
+				<span class="break-words font-inter text-[18.41px] font-medium text-[#cccccc]">{date}</span>
 			</div>
 		</div>
-		<div>
-			<span class="break-words font-inter text-[18.41px] font-medium text-[#cccccc]">{date}</span>
+
+		<div class="inline-flex w-[494.34px] flex-col gap-3">
+			<div>
+				<span
+					class="break-words font-['Inter'] text-[22.09px] font-semibold text-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
+				>
+					{reviewTitle}
+				</span>
+			</div>
+			<div>
+				<span class="break-words font-['Inter'] text-[18.41px] font-medium text-[#cccccc]">
+					{reviewText}
+				</span>
+			</div>
+			<div>
+				<span
+					class="break-words font-['Inter'] text-[18.41px] font-semibold text-white underline drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
+				>
+					Full Review
+				</span>
+			</div>
 		</div>
 	</div>
-	<div class="w-[488px]">
-		<span class="break-words font-inter text-[18.41px] font-medium text-[#cccccc]"
-			>The not-so-secret weapon this CAPTAIN AMERICA has going for it is Harrison Ford. Don't
-			believe the nay-sayers out there: Brave New World is a 21st century Tall Tale, and if it takes
-			two viewings to take it all in, so be it
-		</span>
-	</div>
+	<div class="h-0 self-stretch border-[1.38px] border-[#222222]"></div>
 </div>
