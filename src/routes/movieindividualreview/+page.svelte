73b84fbcca24a791ff42fbe9c30b29ef37b1716b
Jeffrey Law <jeffreylaw@Jeffreys-MacBook-Air.local>
2025-04-15 15:49:14 +0800

    feat: add Movie Individual Review Page

---
+++ src/routes/movieindividualreview/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import IndividualReviewView from '../../views/movieindividualreview/IndividualReviewView.svelte';
+</script>
+
+<div class="m-0 flex min-h-screen w-full flex-col items-center justify-start p-0">
+	<br />
+	<div>
+		<IndividualReviewView />
+	</div>
+</div>
