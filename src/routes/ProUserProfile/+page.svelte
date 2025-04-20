41e88834d3740714f4cd9c7e803b0c913e5d267f
Cliff_HO <hotszhon@gmail.com>
2025-04-21 00:06:11 +0800

    Feat: Add Critics Reviews Draft view

--- src/routes/ProUserProfile/+page.svelte
+++ src/routes/ProUserProfile/+page.svelte
@@ -2,6 +2,7 @@
 	import UserProfileView from '../../views/userprofile/UserProfileView.svelte';
 	import WatchListView from '../../views/userprofile/WatchListView.svelte';
 	import CriticReviewView from '../../views/userprofile/CriticReviewView.svelte';
+	import ReviewDraftView from '../../views/userprofile/ReviewDraftView.svelte';
 </script>
 
 <div>
@@ -12,5 +13,9 @@
 	<br />
 	<br />
 	<CriticReviewView />
+	<br />
+	<br />
+	<ReviewDraftView />
+	<br />
 	<br />
 </div>
---
+++ src/views/userprofile/ReviewDraftView.svelte
@@ -0,0 +1,26 @@
+<script>
+	import ReviewDraftBlock from '../../lib/customComponents/reviewdraft/ReviewDraftBlock.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<div class="relative flex flex-shrink-0 flex-col items-start justify-center gap-8 self-stretch">
+	<div
+		class="relative flex w-[1100px] flex-shrink-0 flex-row items-center justify-between self-stretch"
+	>
+		<!-- Section title with purple line and arrow -->
+		<div class="relative flex flex-shrink-0 flex-row items-center justify-start gap-3">
+			<div class="relative h-[37px] w-[5px] flex-shrink-0 rounded-[2px] bg-[#802bb1]"></div>
+			<div class="relative text-left text-[32px] font-semibold text-white">
+				Critics Reviews Draft
+			</div>
+		</div>
+	</div>
+	<ReviewDraftBlock />
+	<div class="flex w-full justify-center">
+		<Button
+			class="relative flex h-[32px] w-[100px] flex-shrink-0 cursor-pointer flex-row items-center justify-center gap-[10px] rounded-[4px] bg-[#802bb1] font-bold text-white"
+		>
+			Load More
+		</Button>
+	</div>
+</div>
