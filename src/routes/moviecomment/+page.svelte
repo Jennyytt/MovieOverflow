614f551030e889f4c6bce7c821154d7102afbd9b
Jennytt <jennytoy7661@gmail.com>
2025-04-23 00:02:17 +0800

    fix: main content layout

--- src/routes/moviecomment/+page.svelte
+++ src/routes/moviecomment/+page.svelte
@@ -2,10 +2,8 @@
 	import MovieCommentView from '../../views/moviecomment/MovieCommentView.svelte';
 </script>
 
-<div class="m-0 flex min-h-screen w-full flex-col items-center justify-start p-0">
-	<br />
-	<br />
-	<div>
+<div class="m-0 flex min-h-screen w-full flex-col items-center p-0">
+	<div class="mt-8">
 		<MovieCommentView />
 	</div>
 </div>
