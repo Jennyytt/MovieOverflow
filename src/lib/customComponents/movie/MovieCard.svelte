0733371234a77274be8995396bb8586cb2d5b052
Jennytt <jennytoy7661@gmail.com>
2025-04-24 23:27:23 +0800

    fix: rating range and duration

--- src/lib/customComponents/movie/MovieCard.svelte
+++ src/lib/customComponents/movie/MovieCard.svelte
@@ -13,12 +13,12 @@
 		<!-- rating-->
 		<div class="flex basis-1/2 items-center justify-center">
 			<Star class="relative h-4 w-5 pr-1" fill="#FBC02D" color="#FBC02D" />
-			<div class="font-Inter font-bold text-white">7.5</div>
+			<div class="font-Inter font-bold text-white">4.1</div>
 		</div>
 		<!-- duration -->
 		<div class="flex basis-1/2 items-center justify-center">
 			<Clock class="h-4 w-5 pr-1" color="#FBC02D" />
-			<div class="font-Inter font-bold text-white">7.5</div>
+			<div class="font-Inter font-bold text-white">1h 55m</div>
 		</div>
 	</div>
 	<div>
