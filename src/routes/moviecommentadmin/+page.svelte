461881ab676e6da10c4d8b5b7ac8de270f3689ce
Jeffrey Law <jeffreylaw@Jeffreys-MacBook-Air.local>
2025-04-24 22:30:20 +0800

    fix: main content layout

--- src/routes/moviecommentadmin/+page.svelte
+++ src/routes/moviecommentadmin/+page.svelte
@@ -2,10 +2,8 @@
 	import MovieCommentAdminView from '../../views/moviecomment/MovieCommentAdminView.svelte';
 </script>
 
-<div class="m-0 flex min-h-screen w-full flex-col items-center justify-start p-0">
-	<br />
-	<br />
-	<div>
+<div class="m-0 flex min-h-screen w-full flex-col items-center p-0">
+	<div class="mt-8">
 		<MovieCommentAdminView />
 	</div>
 </div>
