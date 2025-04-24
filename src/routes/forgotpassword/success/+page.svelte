dadf4d5f22357b44dc0965a0968481da4c3ca782
Jenny <125589002+Jennyytt@users.noreply.github.com>
2025-04-24 22:48:58 +0800

    Revert "Revert "UI/login pull request ""

---
+++ src/routes/forgotpassword/success/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import PasswordResetBox from '../../../lib/customComponents/userauthen/SuccessMessage.svelte';
+	
+
+	
+</script>
+
+<div>
+	<PasswordResetBox
+  messages={[
+    "Congratulations! Your Password has been changed.",
+	"Click “Log in” to log in with your new password."
+  ]}
+/>
+	<br />
+</div>
