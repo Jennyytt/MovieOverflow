d61766099b8e4f6f884125ca1121f2b956896334
Jennytt <jennytoy7661@gmail.com>
2025-05-11 16:56:34 +0800

    fix: carousel number and watch other movies arrow

--- src/views/movieprofile/WatchOtherMoviesView.svelte
+++ src/views/movieprofile/WatchOtherMoviesView.svelte
@@ -1,7 +1,5 @@
 <script>
 	import MovieCarousel from '$lib/customComponents/movie/MovieCarousel.svelte';
-	import { ChevronRight } from 'radix-icons-svelte';
-	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
 <div class="relative flex flex-shrink-0 flex-col items-start justify-center gap-8 self-stretch">
@@ -12,14 +10,6 @@
 		<div class="relative flex flex-shrink-0 flex-row items-center justify-start gap-3">
 			<div class="relative h-[37px] w-[5px] flex-shrink-0 rounded-[2px] bg-[#802bb1]"></div>
 			<div class="relative text-left text-[32px] font-semibold text-white">Watch Other Movies</div>
-			<Button
-				variant="ghost"
-				size="icon"
-				class="h-auto w-auto p-0 hover:bg-transparent focus-visible:ring-0"
-				><ChevronRight
-					class="relative h-[23px] w-[23px] flex-shrink-0 font-bold text-white"
-				/></Button
-			>
 		</div>
 	</div>
 
