ab171862f3f07e7997a2a196ce32c007a1f6e326
Jennytt <jennytoy7661@gmail.com>
2025-05-12 02:32:51 +0800

    fix: syntax error of WatchList

--- src/routes/userprofile/+page.svelte
+++ src/routes/userprofile/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import UserProfileView from '../../views/userprofile/UserProfileView.svelte';
-	import WatchlistView from '../../views/userprofile/WatchListView.svelte';
+	import WatchlistView from '../../views/userprofile/WatchlistView.svelte';
 	import CriticReviewView from '../../views/userprofile/CriticReviewView.svelte';
 	import ReviewDraftView from '../../views/userprofile/ReviewDraftView.svelte';
 	import UserCommentView from '../../views/userprofile/UserCommentView.svelte';
