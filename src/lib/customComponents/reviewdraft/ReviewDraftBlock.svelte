58d67f6ec1ede354bb76dc9d7f92c18bd9c3044e
c480-lemon <florindadaluz@gmail.com>
2025-05-12 18:18:08 +0800

    Feat: Add Critics Reviews Draft view

---
+++ src/lib/customComponents/reviewdraft/ReviewDraftBlock.svelte
@@ -0,0 +1,19 @@
+<script>
+	import ReviewDraftCard from './ReviewDraftCard.svelte';
+	import { Card } from '$lib/components/ui/card';
+</script>
+
+<Card
+	class="margin-top-[20px] relative box-border w-[1100px] overflow-hidden rounded-[10px] border-[#222222] bg-[#000000]"
+>
+	<div
+		class="margin-top-[20px] relative flex w-[1100px] flex-shrink-0 flex-col items-center justify-between gap-[10px] self-stretch"
+	>
+		<br />
+		<ReviewDraftCard />
+		<div class="relative h-[1px] w-[1048px] bg-[#222222]"></div>
+		<ReviewDraftCard />
+		<div class="relative h-[1px] w-[1048px] bg-[#222222]"></div>
+		<ReviewDraftCard />
+	</div>
+</Card>
---
+++ src/routes/ProUserProfile/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import UserProfileView from '../../views/userprofile/UserProfileView.svelte';
+	import WatchListView from '../../views/userprofile/WatchListView.svelte';
+	import CriticReviewView from '../../views/userprofile/CriticReviewView.svelte';
+	import ReviewDraftView from '../../views/userprofile/ReviewDraftView.svelte';
+</script>
+
+<div>
+	<UserProfileView />
+	<br />
+	<br />
+	<WatchListView />
+	<br />
+	<br />
+	<CriticReviewView />
+	<br />
+	<br />
+	<ReviewDraftView />
+	<br />
+	<br />
+</div>
