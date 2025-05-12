d8d76791afc19756c20c33315e3868159b2370b4
Jeffrey Law <jeffreylaw@Jeffreys-MacBook-Air.local>
2025-05-13 04:08:25 +0800

    feature: add all features of backend/review

---
+++ src/routes/moviecriticsreview/[movieId]/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import CriticsReviewView from '../../../views/moviecriticsreview/CriticsReviewView.svelte';
+	export let data;
+</script>
+
+{#if data.error}
+	<div class="flex h-screen items-center justify-center">
+		<div class="rounded bg-red-500 p-6 text-white">
+			<h2 class="text-2xl font-bold">Error Loading Reviews</h2>
+			<p>{data.error}</p>
+		</div>
+	</div>
+{:else}
+	<div class="m-0 flex min-h-screen w-full flex-col items-center p-0">
+		<div class="mt-8">
+			<CriticsReviewView {data} />
+		</div>
+	</div>
+{/if}
