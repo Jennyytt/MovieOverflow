e442abe7ea3524b902e2789698a0df4517d45c33
c480-lemon <florindadaluz@gmail.com>
2025-05-12 18:28:03 +0800

    fix: resolve conflicts in user profile views

--- src/routes/userprofile/+page.svelte
+++ src/routes/userprofile/+page.svelte
@@ -1,23 +1,26 @@
 <script>
 	import UserProfileView from '../../views/userprofile/UserProfileView.svelte';
-	import WatchListView from '../../views/userprofile/WatchListView.svelte';
+	import WatchlistView from '../../views/userprofile/WatchListView.svelte';
 	import CriticReviewView from '../../views/userprofile/CriticReviewView.svelte';
 	import ReviewDraftView from '../../views/userprofile/ReviewDraftView.svelte';
 	import UserCommentView from '../../views/userprofile/UserCommentView.svelte';
+	let ProUser = true; // Set this to true if the user is a pro user
 </script>
 
 <div class="mt-8">
-	<UserProfileView />
+	<UserProfileView {ProUser} />
 	<br />
 	<br />
-	<WatchListView />
+	<WatchlistView />
 	<br />
 	<br />
-	<CriticReviewView />
-	<br />
-	<br />
-	<ReviewDraftView />
-	<br />
-	<br />
+	{#if ProUser}
+		<CriticReviewView />
+		<br />
+		<br />
+		<ReviewDraftView />
+		<br />
+		<br />
+	{/if}
 	<UserCommentView />
 </div>
