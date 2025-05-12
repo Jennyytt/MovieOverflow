f54780309a3e07217861fef86ecc88bd60475220
c480-lemon <florindadaluz@gmail.com>
2025-05-12 22:03:59 +0800

    fix: fix the bug of not updating "verified" status after clicking verifcation email

--- src/routes/signup/success/+page.svelte
+++ src/routes/signup/success/+page.svelte
@@ -1,5 +1,27 @@
 <script>
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
 	import SignUpSuccess from '../../../lib/customComponents/userauthen/SuccessMessage.svelte';
+	import pb from '$lib/pb';
+
+	let token = '';
+	let verified = false;
+	let failed = false;
+
+	$: token = $page.url.searchParams.get('token');
+
+	onMount(async () => {
+  console.log("Token from URL:", token);
+  try {
+    await pb.collection('users').confirmVerification(token);
+    console.log("Verification succeeded");
+    verified = true;
+  } catch (err) {
+    console.error("Verification failed:", JSON.stringify(err?.response?.data || err, null, 2)); //for debug
+
+  }
+});
+
 </script>
 
 <div>
